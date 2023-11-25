be02e17f04a0aa07c5c6f5bc45b57ce51aecd9e2
Vanchenko <66661914+Vanchenko@users.noreply.github.com>
2023-11-25 13:43:35 +0000

    Deploying to gh-pages from @ WOWmasterJulia/Ex-Moto_parcel@93e4bb8310743bb9c07e0715867a949ffda34204 🚀

---
+++ index.c7aba49e.js
@@ -0,0 +1,2 @@
+var swiperOne=new Swiper(".main-swiper",{spaceBetween:30,centeredSlides:!0,autoplay:{delay:2500,disableOnInteraction:!1},pagination:{el:".swiper-pagination"}}),swiperTwo=new Swiper(".feedback-swiper",{spaceBetween:30,centeredSlides:!0,autoplay:{delay:2500,disableOnInteraction:!1},pagination:{el:".swiper-pagination"}});
+//# sourceMappingURL=index.c7aba49e.js.map
