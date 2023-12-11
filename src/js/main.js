4aa7984c9feae5424e120aeefed56d27273119d8
Vanchenko <vanchenkoev@pakline.com.ua>
2023-12-11 22:52:37 +0200

    link to PDF and square active choice

--- src/js/main.js
+++ src/js/main.js
@@ -76,3 +76,28 @@ const closeMenuBtn = document.querySelector("[data-modal-close]");
      xx.classList.remove('hiddens');
    }
  }); 
+
+ // ++++++++++ выбор квадрата в боковом меню справа +++++++++++++++++++
+ const LinkSqrActiveEl = document.querySelector('.page-section');
+ //console.log('LinkSqrActiveEl',LinkSqrActiveEl.dataset.action);
+ 
+ switch (LinkSqrActiveEl.dataset.action) {
+  case 'srv-3': document.querySelector('#srv-3').classList.add('active');
+    break;
+  case 'srv-2': document.querySelector('#srv-2').classList.add('active');
+    break;  
+  case 'srv-4': document.querySelector('#srv-4').classList.add('active');
+    break;
+  case 'srv-5': document.querySelector('#srv-5').classList.add('active');
+    break;  
+  case 'srv-6': document.querySelector('#srv-6').classList.add('active');
+    break;
+  case 'srv-7': document.querySelector('#srv-7').classList.add('active');
+    break;
+  case 'srv-8': document.querySelector('#srv-8').classList.add('active');
+    break;
+  case 'srv-9': document.querySelector('#srv-9').classList.add('active');
+    break;
+  case 'srv-0': document.querySelector('#srv-0').classList.add('active');
+    break; 
+ }
